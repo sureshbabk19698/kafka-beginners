5a397274fe5d6235c97f637e27bdfdf4c487f0d2
Suresh Babu K <85051490+sureshbabk19698@users.noreply.github.com>
2024-02-11 14:29:45 +0530

    Kafka learning

--- kafka-basics/src/main/java/org/sk/config/KafkaConfig.java
+++ kafka-basics/src/main/java/org/sk/config/KafkaConfig.java
@@ -16,7 +16,7 @@ public class KafkaConfig {
             props.put("bootstrap.servers", "https://gentle-macaw-10295-us1-kafka.upstash.io:9092");
             props.put("sasl.mechanism", "SCRAM-SHA-256");
             props.put("security.protocol", "SASL_SSL");
-            props.put("sasl.jaas.config", "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"Z2VudGxlLW1hY2F3LTEwMjk1JM9RJYJKIkuF54sZmv5dIuwfLnQ8yRrgMOXSzQI\" password=\"ZTMxMTA2MzUtZTI2ZC00MGQ5LWExZDUtZDVhYTM3YzkwNGVi\";");
+            props.put("sasl.jaas.config", "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"***\" password=\"**");
         }
         props.put("key.serializer", StringSerializer.class.getName());
         props.put("value.serializer", StringSerializer.class.getName());
@@ -35,7 +35,6 @@ public class KafkaConfig {
             props.put("security.protocol", "SASL_SSL");
             // Provide your username and password
             props.put("sasl.jaas.config", "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"***\" password=\"**");
-
         }
         props.put("key.deserializer", StringDeserializer.class.getName());
         props.put("value.deserializer", StringDeserializer.class.getName());
