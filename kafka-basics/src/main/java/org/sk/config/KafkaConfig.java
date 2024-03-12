6ef220ff9b6105f9edffe9299bd848dd434d6dea
Suresh Babu K <85051490+sureshbabk19698@users.noreply.github.com>
2024-03-12 12:23:29 +0530

    Updated Readme file

--- kafka-basics/src/main/java/org/sk/config/KafkaConfig.java
+++ kafka-basics/src/main/java/org/sk/config/KafkaConfig.java
@@ -1,6 +1,7 @@
 package org.sk.config;
 
-import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
@@ -16,10 +17,10 @@ public class KafkaConfig {
             props.put("bootstrap.servers", "https://gentle-macaw-10295-us1-kafka.upstash.io:9092");
             props.put("sasl.mechanism", "SCRAM-SHA-256");
             props.put("security.protocol", "SASL_SSL");
-            props.put("sasl.jaas.config", "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"***\" password=\"**");
+            props.put("sasl.jaas.config", "");
         }
-        props.put("key.serializer", StringSerializer.class.getName());
-        props.put("value.serializer", StringSerializer.class.getName());
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 //        props.put("partitioner.class", RoundRobinPartitioner.class.getName()); // Less number of batches leads to more requests to partitions
 //        props.put("batch.size", "200"); // default batch.size = 16384 // mores number of batches leads to less requests to partitions -- StickyPartitioner
         return props;
@@ -34,12 +35,12 @@ public class KafkaConfig {
             props.put("sasl.mechanism", "SCRAM-SHA-256");
             props.put("security.protocol", "SASL_SSL");
             // Provide your username and password
-            props.put("sasl.jaas.config", "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"***\" password=\"**");
+            props.put("sasl.jaas.config", "");
         }
-        props.put("key.deserializer", StringDeserializer.class.getName());
-        props.put("value.deserializer", StringDeserializer.class.getName());
-        props.put("group.id", "g1");
-        props.put("auto.offset.reset", "earliest");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "g1");
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         return props;
     }
 
